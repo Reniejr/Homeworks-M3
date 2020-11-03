7075c9555f2af3e08e0f82092fe09b00157ec52e
Reniejr <reniejr@gmail.com>
2020-11-03 16:59:09 +0100

    update

---
+++ M3-D2/Spotify/JS/artist.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+
+    let pageTitle = document.getElementById('artist-page-title')
+    let artistContent = document.getElementById('artist-content')
+    /*
+    pageTitle.innerText = `${artistData.name}`
+    for(let album of parsedJson.data){
+        let contentResult = `
+        <div class='artist-content row'>
+            <div class='artist-search-result col-12 col-sm-6 col-md-4 col-lg-3'>
+            <img src='${album.album.cover_small}'>
+            <p>${album.album.title}</p>
+            <p>${album.title_short}</p>
+            </div>
+        </div>
+        `
+        artistContent.appendChild(contentResult)
+    }*/
+    
+
+}
