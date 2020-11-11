a8ffa2a602fde04a62ee701f8b4308baa18d10f8
Reniejr <reniejr@gmail.com>
2020-11-11 13:51:20 +0100

    update

--- M3-D2/Spotify/JS/login.js
+++ M3-D2/Spotify/JS/login.js
@@ -1,6 +1,13 @@
 const loginBtn = document.getElementById('login-btn')
-const client_id = 'e54184c2056e4fceba268bad7ae4175f';
-const secret = '90bf6fe6367b4d92b75aa2bca47478f0'
-let auth__ecripted = 'Basic ' + btoa(`${client_id}:${secret}`)
-let access_token = ""
-let refresh_token =""
+const client_id = '6cee7a5f42ec413c8af5c31b8038445e';
+const secret = '4f9d04fc510e49c5b228e904da1adfd1'
+const redirect_uri = `http%3A%2F%2F127.0.0.1%3A5500%2Fcallback.html`
+
+const authentication = () => {
+    const scopes = 'user-read-private user-read-email user-top-read'
+    window.location.href = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}&scope=${scopes}&state=34fFs29kd09&show_dialog=true`
+}
+
+window.onload = ()=>{
+    loginBtn.addEventListener('click', authentication)
+}
